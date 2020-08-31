100cf72c4cac836ac1d7054611d10031cda9ee6b
Retrone1250 <carlos.lopez.elorduy2@gmail.com>
2020-08-31 12:15:29 +0200

    Changed color class

--- survival_role.py
+++ survival_role.py
@@ -1,40 +1,49 @@
 # In the morning, at the kitchen, at the time
 """ import sys """
-class color:
-   PURPLE = '\033[95m'
-   CYAN = '\033[96m'
-   DARKCYAN = '\033[36m'
-   BLUE = '\033[94m'
-   BLACK = "\033[30m"
-   LIGHTGRAY = "\033[37m"
-   DARKGRAY = "\033[90m"
-   GREEN = '\033[92m'
-   YELLOW = '\033[93m'
-   RED = '\033[91m'
-   LIGHTRED = "\033[91m"
-   WHITE = "\033[97m"
-   BOLD = '\033[1m'
-   BACKGROUNDWHITE = "\033[107m"
-   BACKGROUNDDEFAULT      = "\033[49m"
-   BACKGROUNDBLACK      = "\033[40m"
-   BACKGROUNDRED        = "\033[41m"
-   BACKGROUNDGREEN        = "\033[42m"
-   BACKGROUNDYELLOW       = "\033[43m"
-   BACKGROUNDBLUE     = "\033[44m"
-   RESETBOLD = "\033[21m"
-   UNDERLINE = '\033[4m'
-   DIM = "\033[2m"
-   BLINK = "\033[5m"
-   REVERSE = "\033[7m"
-   HIDDEN = "\033[8m"
-   DEFAULT = "\033[39m"
-   END = '\033[0m'
+
+# =======================
+# === START VARIABLES ===
+# =======================
+
+PURPLE = '\033[95m'
+CYAN = '\033[96m'
+DARKCYAN = '\033[36m'
+BLUE = '\033[94m'
+BLACK = "\033[30m"
+LIGHTGRAY = "\033[37m"
+DARKGRAY = "\033[90m"
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+LIGHTRED = "\033[91m"
+WHITE = "\033[97m"
+BOLD = '\033[1m'
+BACKGROUNDWHITE = "\033[107m"
+BACKGROUNDDEFAULT      = "\033[49m"
+BACKGROUNDBLACK      = "\033[40m"
+BACKGROUNDRED        = "\033[41m"
+BACKGROUNDGREEN        = "\033[42m"
+BACKGROUNDYELLOW       = "\033[43m"
+BACKGROUNDBLUE     = "\033[44m"
+RESETBOLD = "\033[21m"
+UNDERLINE = '\033[4m'
+DIM = "\033[2m"
+BLINK = "\033[5m"
+REVERSE = "\033[7m"
+HIDDEN = "\033[8m"
+DEFAULT = "\033[39m"
+END = '\033[0m'
 
 LIFE_POINTS = 10000
 
-print(color.DARKCYAN+"Welcome to the Bear Grylls Ultimate Survival Skills game.\nHere you will have to pass a series of levels that will challenge your abilities of surviving in the wild with very few resources.\nGood luck!")
-print("Please select 3 of the following items to put in your backpack:"+color.END)
-print(color.LIGHTRED+"{}\t{}\t{}\n{}\t{}\t{}\n{}\t{}\t{}\n".format('Knife', 'Stormproof match kit', 'Flashlight', 'Bucket', 'Mosquito repeller', 'Tent', 
+
+# =============
+# === LOBBY ===
+# =============
+
+print(DARKCYAN+"Welcome to the Bear Grylls Ultimate Survival Skills game.\nHere you will have to pass a series of levels that will challenge your abilities of surviving in the wild with very few resources.\nGood luck!")
+print("Please select 3 of the following items to put in your backpack:"+END)
+print(LIGHTRED+"{}\t{}\t{}\n{}\t{}\t{}\n{}\t{}\t{}\n".format('Knife', 'Stormproof match kit', 'Flashlight', 'Bucket', 'Mosquito repeller', 'Tent', 
     'Sleeping bag', 'GPS', 'Water filter bottle'))
 possible_items = ['Knife', 'Stormproof match kit', 'Flashlight', 'Bucket', 'Mosquito repeller', 'Tent', 
     'Sleeping bag', 'GPS', 'Water filter bottle']
@@ -56,7 +65,7 @@ if item3 in possible_items:
 ready_to_start = 0
 while ready_to_start == False:
     if len(backpack) == 3:
-        print(color.DARKCYAN+'Good! You are ready to begin the adventure! This are the items in your backpack to begin with:')
+        print(DARKCYAN+'Good! You are ready to begin the adventure! This are the items in your backpack to begin with:')
         for i in backpack:
             print(i)
         ready_to_start = 1
@@ -72,10 +81,10 @@ while ready_to_start == False:
                     for i in backpack:
                         print(i)
                     ready_to_start = 1
-print(color.BOLD+'LIFE POINTS: {}.'.format(LIFE_POINTS)+color.END)
-print(color.BACKGROUNDBLUE+color.BLACK+"LEVEL1"+color.END+color.DARKCYAN)
+print(BOLD+'LIFE POINTS: {}.'.format(LIFE_POINTS)+END)
+print(BACKGROUNDBLUE+BLACK+"LEVEL1"+END+DARKCYAN)
 print("You are in Manning. This is a valley with poor biodiversity so you have to get out of here and find a place with resources that will ensure your survival.")
-print(color.LIGHTRED+"Where do you choose to go?\n{}\n{}\n(Input name only)".format("Cosima -- Land of fairies", "Griffin -- Land of mythological creatures"))
+print(LIGHTRED+"Where do you choose to go?\n{}\n{}\n(Input name only)".format("Cosima -- Land of fairies", "Griffin -- Land of mythological creatures"))
 location1 = input()
 follow = 0
 while follow == 0:
@@ -86,7 +95,7 @@ while follow == 0:
         location1 = input()
 
 if location1 == 'Cosima':
-    print(color.DARKCYAN+"Good choice!\nAfter a few days walking you seem to have found a nice green grassland. You walk around and discover a huge diversity of fruits and plants that you have never seen before.")
+    print(DARKCYAN+"Good choice!\nAfter a few days walking you seem to have found a nice green grassland. You walk around and discover a huge diversity of fruits and plants that you have never seen before.")
     print('There is even a river with crystalline water. You deserve drinking so you stop for a bit and enjoy yourself.')
     a = """
     Hey! Wake up! You fell asleep. It is time to get up and keep discovering what's new. Oh, wait. Something looks wrong. You got some rashes on your arms.
@@ -94,8 +103,8 @@ if location1 == 'Cosima':
     the name but you do remember that the fruit was red and triangular."""
     print(a)
     LIFE_POINTS = LIFE_POINTS - 500
-    print(color.BOLD+'LIFE POINTS: {}.'.format(LIFE_POINTS)+color.END)
-    print(color.LIGHTRED+'Where will you go?\n{}\n{}\n(Input capital letter only)'.format("A - Away from the river", 'B - Cross the river '))
+    print(BOLD+'LIFE POINTS: {}.'.format(LIFE_POINTS)+END)
+    print(LIGHTRED+'Where will you go?\n{}\n{}\n(Input capital letter only)'.format("A - Away from the river", 'B - Cross the river '))
     direction1 = input()
     follow = 0
     while follow == 0:
@@ -114,9 +123,9 @@ if location1 == 'Cosima':
         Bingo! These trees have fruits. Now is time for you to find the red and triangular fruit that professor Layton mentioned in class. There
         is one tree with a similar fruit. You take a few and eat one carefully.
          """
-        print(color.DARKCYAN+a+b)
+        print(DARKCYAN+a+b)
         LIFE_POINTS = LIFE_POINTS + 200
-        print(color.BOLD+'LIFE POINTS: {}.'.format(LIFE_POINTS)+color.END)
+        print(BOLD+'LIFE POINTS: {}.'.format(LIFE_POINTS)+END)
         """ introduce fairies """
     if direction1 == 'B':
         LIFE_POINTS = LIFE_POINTS - 500
@@ -124,8 +133,8 @@ if location1 == 'Cosima':
         You are crossing the river and thus water is in contact with you. You are hurrying to get to the other side because water seems to be
         slightly poisonous.  On the other side of the river the landscape is just the same. However, it smells somewhat different. You shall trace 
         that smell and find out where it comes from!"""
-        print(color.DARKCYAN+a)
-        print(color.BOLD+'LIFE POINTS: {}.'.format(LIFE_POINTS)+color.END)
+        print(DARKCYAN+a)
+        print(BOLD+'LIFE POINTS: {}.'.format(LIFE_POINTS)+END)
         a = """ 
         The smell is definitely coming from this tree in front of you right now.  """
 
